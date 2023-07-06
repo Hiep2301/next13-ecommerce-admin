cdb110c9ccb3c567ab84e0a339885109fbd53a72
hiep2301 <ngotranhiep2015@gmail.com>
2023-07-06 12:26:26 +0700

    up

--- app/(dashboard)/[storeId]/(routes)/products/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/page.tsx
@@ -1,7 +1,7 @@
 import { format } from "date-fns";
 
 import prismadb from "@/lib/prismadb";
-import { formatter } from "@/lib/utils";
+import { formatter } from "@/lib/utils"; 
 
 import { ProductsClient } from "./components/client";
 import { ProductColumn } from "./components/columns";
@@ -39,7 +39,7 @@ const ProductsPage = async ({
 
   return (
     <div className="flex-col">
-      <div className="flex-1 space-y-4 p-8 pt-6">
+      <div className="flex-1 p-8 pt-6 space-y-4">
         <ProductsClient data={formattedProducts} />
       </div>
     </div>
